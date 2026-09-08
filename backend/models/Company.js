de2b1dc777dc71850730bd73a31e3d09a90534cd
Rohit Singh <rohit.singh43@outlook.com>
2026-09-08 16:22:46 +0530

    feat: create company model/schema

---
+++ backend/models/Company.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    website: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    logo: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    industry: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    companySize: {
+      type: Number,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Company = mongoose.model("Company", companySchema);
+
+export default Company;
