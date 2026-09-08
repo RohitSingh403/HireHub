d83a2b56b62ba3fcf9b774bf7b9b35cc34d84c0b
Rohit Singh <rohit.singh43@outlook.com>
2026-09-08 07:43:12 +0530

    refactor: add allowed roles to access role middleware

--- backend/middleware/roleMiddleware.js
+++ backend/middleware/roleMiddleware.js
@@ -1,15 +1,19 @@
-const roleMiddleware = function (req, res, next) {
-  try {
-    const currentRole = req.user.role;
-    if (currentRole !== "candidate" && currentRole !== "recruiter" && currentRole !== "admin" ) {
-      return res.status(403).json({
-        err: "Not allowed",
+const roleMiddleware = function (allowedRole) {
+  return function (req, res, next) {
+    try {
+      const currentRole = req.user.role;
+      if (currentRole !== allowedRole) {
+        return res.status(403).json({
+          err: "Not allowed",
+        });
+      }
+      next();
+    } catch (err) {
+      return res.status(500).json({
+        err: "Internal server error",
       });
     }
-    next();
-  } catch (err) {
-    return res.status(500).json({
-      err: "Internal server error",
-    });
-  }
+  };
 };
+
+export default roleMiddleware;
