0efbcbb851b375c7c38c08e42cd7b1e645bef239
Rohit Singh <rohit.singh43@outlook.com>
2026-09-15 15:16:57 +0530

    chore(frontend): add auth brand panel

---
+++ frontend/src/features/auth/components/AuthBrandPanel.jsx
@@ -0,0 +1,14 @@
+function AuthBrandPanel() {
+  return (
+    <section className=" bg-blue-50 p-8 md:p-12">
+      <div className="flex items-center gap-2">
+        <img className="h-13 " src="/Hire-Hub_logo.png" alt="logo" />
+        <h1 className="text-2xl font-bold text-slate-900 w-24 h-auto">
+          HireHub
+        </h1>
+      </div>
+    </section>
+  );
+}
+
+export default AuthBrandPanel;
